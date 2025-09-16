b0060b9836d3d67183a5efce3d0a2fe599ff9b7a
Nullskulls <nullskulls@proton.me>
2025-09-16 22:39:50 +0300

    updated example.env and added basic bot db

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,39 @@
-import os
+import os, datetime
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()
+domain = os.environ.get('DOMAIN')
 slack_api_key = os.environ.get("SLACK_API_KEY")
 slack_signing_secret = os.environ.get("SLACK_SIGNING_SECRET")
 socket_id = os.environ.get("SOCKET_ID")
+
+bulk_messages = []
 
 def build_app(api_key, signing_secret):
     app = App(
         token=api_key,
         signing_secret=signing_secret
         )
+
+    @app.event("message")
+    def handle_message(body, client, logger):
+        event = body.get("event", {})
+        if not event:
+            return
+        if event.get("type") == "message":
+            if len(bulk_messages) < 10:
+                data = {
+                    "type": "message",
+                    "text": event.get("text"),
+                    "channel": event.get("channel"),
+                    "ts": event.get("ts"),
+                    "user": event.get("user"),
+                    "time": datetime.datetime.utcnow().isoformat()
+                }
+                bulk_messages.append(data)
+
     return app
 
 
