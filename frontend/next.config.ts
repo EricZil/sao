5fc105a12891f62ca93aaf4f80895c13710543b7
Eric Zilvytis <eric@nezser.com>
2025-09-21 00:09:40 +0300

    slack pics fetish

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['avatars.slack-edge.com', 'ca.slack-edge.com'],
+  },
 };
 
 export default nextConfig;
